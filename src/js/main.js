07136c2e01dc71a26e6615af9d2d16aa4ebab4ed
Angel Orozco <angeldorozcon@gmail.com>
2024-06-26 21:47:16 -0300

    Task: Finishes team activity

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 import ProductData from './ProductData.mjs';
 import ProductListing from './ProductList.mjs';
+import { loadHeaderFooter } from './utils.mjs';
 
 const dataSource = new ProductData('tents');
 const listing = new ProductListing('Tents', dataSource, '.product-list');
 
 listing.init();
+loadHeaderFooter();
