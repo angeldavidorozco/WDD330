e7d33a7aa536df2f2995a584bc99c50af71520d4
Angel Orozco <angeldorozcon@gmail.com>
2024-06-29 17:32:00 -0300

    Task: Finishes individual activity W3

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -4,24 +4,39 @@ export default class ProductListing{
     constructor(category, dataSource, listElement) {
         this.category = category;
         this.dataSource = dataSource;
-        this.listElement = document.querySelector(listElement);
+        this.listElement = listElement;
     }
 
     productCardTemplate(product) {
+        let upperCaseTitle;
+
         if (product.Id === "989CG" || product.Id === "880RT"){
             return 
         }
-        return `<li class="product-card">
-          <a href="product_pages/index.html?product=${product.Id}">
-            <img 
-            src=${product.Image}
-            alt=${product.NameWithoutBrand}
-            >
-            <h3 class="card__brand">${product.Brand.Name}</h3>
-            <h2 class="card__name">${product.Name}</h2>
-            <p class="product-card__price">$${product.FinalPrice}</p>
-          </a>
-        </li>`
+
+        if (product.Category === 'sleeping-bags'){
+            upperCaseTitle = 'Sleeping Bags';
+        }else{
+            const title = product.Category;
+            upperCaseTitle = title.charAt(0).toUpperCase() + title.slice(1);
+        }
+        
+        return `
+        <h2>Top Products ${upperCaseTitle}</h2>
+        <ul class="product-list">
+            <li class="product-card">
+                <a href="product_pages/index.html?product=${product.Id}">
+                    <img 
+                    src=${product.Images.PrimaryMedium}
+                    alt=${product.NameWithoutBrand}
+                    >
+                    <h3 class="card__brand">${product.Brand.Name}</h3>
+                    <h2 class="card__name">${product.Name}</h2>
+                    <p class="product-card__price">$${product.FinalPrice}</p>
+                </a>
+            </li>
+        </ul>
+        `
     }
 
     renderList(list){
@@ -29,7 +44,7 @@ export default class ProductListing{
     }
 
     async init() {
-        const list = await this.dataSource.getData();
+        const list = await this.dataSource.getData(this.category);
         this.renderList(list);
     }
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,3 @@
-import ProductData from './ProductData.mjs';
-import ProductListing from './ProductList.mjs';
 import { loadHeaderFooter } from './utils.mjs';
 
-const dataSource = new ProductData('tents');
-const listing = new ProductListing('Tents', dataSource, '.product-list');
-
-listing.init();
 loadHeaderFooter();
